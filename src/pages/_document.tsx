8410cbb562f28a864ebaa1205f8690b03b0dce81
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2024-05-16 21:50:55 +0700

    adding firebase client

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,3 @@
-import { ChakraProvider } from "@chakra-ui/react";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
@@ -6,10 +5,8 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
-        <ChakraProvider>
-          <Main />
-          <NextScript />
-        </ChakraProvider>
+        <Main />
+        <NextScript />
       </body>
     </Html>
   );
