61cab162855123bd670607696c71d72f28a35de9
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2024-05-17 23:54:03 +0700

    Adding jsonwebtoken

--- src/test/ElipticEncoder.test.ts
+++ src/test/ElipticEncoder.test.ts
@@ -16,13 +16,13 @@ describe("Eliptic curve encoder", () => {
   });
 
   it("should able to encode and decode it correctly", () => {
-    const encoder = new ElipticEncoder(new BrainpoolP512r1(), 32);
+    const encoder = new ElipticEncoder(new BrainpoolP512r1(), 40);
     const textEncoder = new TextEncoder();
 
     const msg = textEncoder.encode(`Halo, Dunia. Apa kabarnya hari ini?`);
     const result = encoder.encode(msg);
 
-    expect(result.length).toBe(2);
+    expect(result.length).toBe(1);
 
     const decoded = encoder.decode(result);
     expect(decoded).toStrictEqual(msg);
