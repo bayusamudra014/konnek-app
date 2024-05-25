7fdbca8d8017d27178ee4cac9d0e768797dba006
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2024-05-25 17:04:56 +0700

    adding some controller

--- src/api/certificate.ts
+++ src/api/certificate.ts
@@ -1,16 +1,18 @@
 import "server-only";
-import { Cache } from "memory-cache";
+import NodeCache from "node-cache";
 import { Certificate, decodeCertificate } from "@/lib/crypto/Certificate";
 import storageAdmin from "./storage";
 
-const cache = new Cache<string, Certificate>();
+const cache = new NodeCache();
 const CACHE_TIME = parseInt(process.env.NEXT_PUBLIC_CACHE_TIME ?? "30_000");
 
 const userBucket = storageAdmin.bucket();
 
-export async function getCertificate(userId: string) {
+export async function getCertificate(
+  userId: string
+): Promise<Certificate | null> {
   if (cache.get(userId)) {
-    return cache.get(userId);
+    return cache.get(userId) ?? null;
   }
 
   if (/^[a-zA-Z0-9-\_]+$/g.test(userId) !== true) {
@@ -23,7 +25,7 @@ export async function getCertificate(userId: string) {
   }
 
   const certificate = await decodeCertificate(downloaded, "raw");
-  cache.put(userId, certificate, CACHE_TIME);
+  cache.set(userId, certificate, CACHE_TIME);
 
   return certificate;
 }
--- src/lib/crypto/const.ts
+++ src/lib/crypto/const.ts
@@ -1,2 +1,3 @@
 export const SIGN_ALGORITHM = "Schnorr_BrainpoolP512r1_SHA512";
 export const ENC_ALGORITHM = "Elgamal_BrainpoolP512r1_40";
+export const CA_NAME = "master_ca";
